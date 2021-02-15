37788a42c33cb6e874cb3a66e1ea287ce769d56b
Jason F <fjord1321@gmail.com>
2021-02-15 08:19:56 -0500

    Created Bits.java and BitsTest.java

---
+++ src/Bits.java
@@ -0,0 +1,81 @@
+import java.util.BitSet;
+
+public class Bits {
+
+    BitSet bt = new BitSet();
+
+    public void BitSet(int nbits){
+        try {
+            bt = new BitSet(nbits);
+            System.out.println(bt.toString());
+        } 
+        catch (NegativeArraySizeException err) {
+            System.out.println(err.toString());
+        }
+    }
+    // Creates a bit set whose initial size is large enough to explicitly
+    // represent bits with indices in the range 0 through nbits-1. All bits are initially false.
+    // Parameters: nbits - the initial size of the bit set
+    // Throws: NegativeArraySizeException - if the specified initial size is negative
+    public void set(int bitIndex){
+
+        try {
+                bt.set(bitIndex, true);
+                System.out.println(bt.toString() + " set at " + bitIndex);
+
+            } 
+            catch (IndexOutOfBoundsException err) {
+                System.out.println(err.toString());
+            }
+    }
+    //Sets the bit at the specified index to true.
+    //Parameters: bitIndex - a bit index
+    //Throws: IndexOutOfBoundsException - if the specified index is negative
+    public boolean get(int bitIndex){
+        try {
+            bt.get(bitIndex);
+            System.out.println("got at " + 5);
+            return true;
+        } 
+        catch (IndexOutOfBoundsException err) {
+            System.out.println(err.toString());
+            return false;
+        }    
+    }
+    //Returns the value of the bit with the specified index. The value
+    //is true if the bit with the index bitIndex is
+    //currently set in this BitSet; otherwise, the result is false.
+    //Parameters: bitIndex - the bit index
+    //Returns: the value of the bit with the specified index
+    //Throws: IndexOutOfBoundsException - if the specified index is negative
+    public void flip(int bitIndex){
+        try {
+            bt.flip(bitIndex);
+            System.out.println("flipped to " + bitIndex);
+
+        } 
+        catch (IndexOutOfBoundsException err) {
+            System.out.println(err.toString());
+        }    
+    }
+    //Sets the bit at the specified index to the complement of its current value.
+    //Parameters: bitIndex - the index of the bit to flip
+    //Throws: IndexOutOfBoundsException - if the specified index is negative
+
+    public static void main(String[] args) {
+        
+        Bits bit = new Bits();
+        bit.BitSet(5);
+
+        bit.set(2);
+        bit.set(3);
+
+        bit.flip(1);
+        bit.get(5);
+
+        System.out.println(bit.bt);
+
+    }
+}
+
+
---
+++ test/BitsTest.java
@@ -0,0 +1,34 @@
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+public class BitsTest {
+
+	@Test
+	public void testBitSet() throws Exception {
+		Bits bit = new Bits();
+		bit.BitSet(10);
+		assertEquals(bit.bt.toString(), "{}");
+	}
+
+	@Test
+	public void testSet() throws Exception {
+
+	}
+
+	@Test
+	public void testGet() throws Exception {
+
+	}
+
+	@Test
+	public void testFlip() throws Exception {
+
+	}
+
+	@Test
+	public void testMain() throws Exception {
+
+	}
+
+}
