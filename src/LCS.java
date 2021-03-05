e02f82658fd088cf6ed4307e65b0a20320f77482
Jason F <fjord1321@gmail.com>
2021-03-05 13:14:56 -0500

    added comments

--- src/LCS.java
+++ src/LCS.java
@@ -5,7 +5,7 @@ public class LCS {
 /*1*/   int m = x.length(), n = y.length();  //def-use: m, n, opt, x, y
         int[][] opt = new int[m+1][n+1];
         
-/*2*/    for (int i = m-1; i >= 0; i--) {  // defined at the entry of the block
+/*2*/    for (int i = m-1; i >= 0; i--) {  // strings of length > 0 needed to enter for loops
 /*3*/         for (int j = n-1; j >= 0; j--) {
 /*4*/           if (x.charAt(i) == y.charAt(j)) {
 /*5*/              opt[i][j] = opt[i+1][j+1] + 1;
@@ -38,17 +38,5 @@ public class LCS {
      
 
     }
-
-//     Complete the following tasks:
-// 1. Draw a control flow graph for the lcs method. 
-//    Choose something reasonable for the node numbers in the graph.
-// 2. Label the control graph with defs and uses at the appropriate nodes.
-// 3. Compute the all-def-use paths criteria for the graph.
-// 4. Implement a set of JUnit tests that cover as many of the all-def-use 
-//    paths (test requirements) as possible.
-// 5. Reflecting on the JUnit test you have written in the previous task, are there any tests 
-//    that you would have written if you only used your intuition and 
-//    did not follow the all-def-use criteria?
-//    Are there any tests that you ended up writing that you would not have otherwise thought of?
 
 }
