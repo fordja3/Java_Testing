cf7b11508dc0a9f91d4faee2208c83157f698075
Jason F <fjord1321@gmail.com>
2021-03-05 11:54:10 -0500

    Added new test

--- src/LCS.java
+++ src/LCS.java
@@ -2,44 +2,41 @@ public class LCS {
 
     public static String lcs(String x, String y) {
 
-        int m = x.length(), n = y.length();  //def-use: m, n, opt
+/*1*/   int m = x.length(), n = y.length();  //def-use: m, n, opt, x, y
         int[][] opt = new int[m+1][n+1];
         
-        for (int i = m-1; i >= 0; i--) {
-            for (int j = n-1; j >= 0; j--) {
-                if (x.charAt(i) == y.charAt(j)) {
-                    opt[i][j] = opt[i+1][j+1] + 1;
-                }else{
+/*2*/    for (int i = m-1; i >= 0; i--) {  // defined at the entry of the block
+/*3*/         for (int j = n-1; j >= 0; j--) {
+/*4*/           if (x.charAt(i) == y.charAt(j)) {
+/*5*/              opt[i][j] = opt[i+1][j+1] + 1;
+/*6*/           }else{
                     opt[i][j] = Math.max(opt[i+1][j], opt[i][j+1]);
                 }
             }
         }
                 
-        String lcs = "";  //def-use: lcs, i, j
+/*7*/   String lcs = "";  //def-use: lcs, i, j
         int i = 0, j = 0;
         
-        while (i < m && j < n) {
-            if (x.charAt(i) == y.charAt(j)) {
-                lcs += x.charAt(i);
+/*8*/   while (i < m && j < n) {
+/*9*/      if (x.charAt(i) == y.charAt(j)) {
+/*10*/          lcs += x.charAt(i);
                 i++;
                 j++;
-            }else if (opt[i+1][j] >= opt[i][j+1]) {
+/*11*/      }else if (opt[i+1][j] >= opt[i][j+1]) {
                 i++;
-            }else {
+/*12*/      }else {
                 j++;
             }
         }
 
-    return lcs;
+/*13*/  return lcs;
+    
     }
 
     public static void main(String[] args){
-        
-        String a = "GGCACCACG";
-        String b = "ACGGCGGATACG";
+     
 
-        String out = LCS.lcs(a, b);
-        System.out.println(out);
     }
 
 //     Complete the following tasks:
@@ -49,8 +46,9 @@ public class LCS {
 // 3. Compute the all-def-use paths criteria for the graph.
 // 4. Implement a set of JUnit tests that cover as many of the all-def-use 
 //    paths (test requirements) as possible.
-// 5. Reflecting on the JUnit test you have written in the previous task, are there any tests that you would
-//    have written if you only used your intuition and did not follow the all-def-use criteria?
+// 5. Reflecting on the JUnit test you have written in the previous task, are there any tests 
+//    that you would have written if you only used your intuition and 
+//    did not follow the all-def-use criteria?
 //    Are there any tests that you ended up writing that you would not have otherwise thought of?
 
 }
--- test/LCSTest.java
+++ test/LCSTest.java
@@ -8,17 +8,63 @@ public class LCSTest {
 	@Test
 	public void testLcs() throws Exception {
 		
-		String a = "GGCACCACG";
+		// skip first for loop
+		String a = "";
         String b = "ACGGCGGATACG";
 
 		String out = LCS.lcs(a, b);
 
-		assertEquals(out, "GGCAACG") ;
+		assertEquals(out, "") ;
 
-	}
 
-	@Test
-	public void testMain() throws Exception {
+		// skip second for loop
+		String c = "ACGGCGGATACG";
+		String d = "";
+
+		String test = LCS.lcs(c, d);
+
+		assertEquals(test, "") ;
+
+		// skip for loops and while loop
+		String e = "";
+		String f = "";
+
+		String tester = LCS.lcs(e, f);
+
+		assertEquals(tester, "") ;
+
+		// returns nothing because the char are in different cases
+		String g = "RACECAR";
+		String h = "racecar";
+
+		String tst = LCS.lcs(g, h);
+
+		assertEquals(tst, "") ;	
+
+		// runs through and matches all characters
+		String i = "PASSPORT";
+		String j = "TROPSSAP";
+
+		String tstr = LCS.lcs(i, j);
+
+		assertEquals(tstr, "PSSP") ;	
+
+		// returns nothing because no char are in string x are in string y
+		String k = "abcdefg";
+		String l = "hijklmnop";
+
+		String valid = LCS.lcs(k, l);
+
+		assertEquals(valid, "") ;	
+
+		// random non-sense string with various characters and cases
+		String m = "!@#$%^&*()_+{}|:;',.//,.?'1234567890~`;) ";
+		String n = "!@#$%^&*()_+{}|:;',.//,.?'0987654321~` ";
+
+		String st = LCS.lcs(m, n);
+
+		assertEquals(st, "!@#$%^&*()_+{}|:;',.//,.?'0~` ") ;	
+
 
 	}
 
